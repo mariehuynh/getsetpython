eb541f99dc2aee705abfc0ce16bd80a6f8a39f39
Marie Huynh <marie@mariehuynh.com>
2017-07-22 10:48:21 -0700

    cleanup

--- moanaplot.py
+++ moanaplot.py
@@ -19,13 +19,7 @@ pattern = r'''(?x)    # set flag to allow verbose regexps
 
 tokens = nltk.regexp_tokenize(raw,pattern)
 
-
-
-
-#tokens = nltk.word_tokenize(raw)
 lctokens = [w.lower() for w in tokens]
-
-
 
 # ordered by frequency
 from collections import Counter
@@ -34,12 +28,4 @@ c.most_common(100)
 
 #plot
 dispersion_plot(lctokens,['maui','hook','heart','te','fiti','ka','ocean','island',
-  'reef','moana','boat','gramma','tattoo',
-  'cheeeehoooo',
-  'crab','shiny'])
-
-
-# Who is WW really about?
-
-#dispersion_plot(lctokens,['diana','wonder','woman','hippolyta','zeus','steve',
-#  'ares','ludendorff','german','germans', 'doctor', 'poison', 'secretary',])
+  'reef','moana','boat','gramma','tattoo','cheeeehoooo','crab','shiny'])
